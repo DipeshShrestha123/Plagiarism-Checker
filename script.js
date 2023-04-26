b8aafabe6e318fe5977cb608dd670410155af9cb
Dipesh shrestha <110885262+DipeshShrestha123@users.noreply.github.com>
2023-04-27 00:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
 let button = document.querySelector('.btn');
 let input = document.querySelector('.hid');
+let textArea = document.getElementById("textArea");
+
 button.onclick = ()=>{
-    input.click();
+  input.click();
 }
 
+input.addEventListener('change', function() {
+  let file = input.files[0];
+  let fileName = file.name;
+  // console.log(fleName);
+  let reader = new FileReader();
 
-function checkPlagiarism() {
-    // get the contents of the textarea and check for plagiarism
-  }
-
-
-  
-  document.getElementById('fileInput').addEventListener('change', function() {
-    var file = this.files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var contents = e.target.result;
-      document.getElementById('textArea').value = contents;
-    };
-    reader.readAsText(file);
-  });
-
-
-
-
-  const pdfFileInput = document.getElementById('pdf-file-input');
-  const pdfTextarea = document.getElementById('pdf-textarea');
-
-  pdfFileInput.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const pdfData = new Uint8Array(event.target.result);
-      pdfjsLib.getDocument({ data: pdfData }).promise.then((pdf) => {
-        const numPages = pdf.numPages;
-        let pdfText = '';
-
-        for (let i = 1; i <= numPages; i++) {
-          pdf.getPage(i).then((page) => {
-            return page.getTextContent();
-          }).then((textContent) => {
-            textContent.items.forEach((item) => {
-              pdfText += item.str + ' ';
-            });
-
-            if (i === numPages) {
-              pdfTextarea.value = pdfText;
+  reader.onload = function() {
+    let text = "";
+    if (fileName.endsWith(".txt")) {
+      // If the file is a text file, use the contents as is
+      text = reader.result;
+    } else if (fileName.endsWith(".pdf")) {
+      // If the file is a PDF, convert it to text using pdf.js
+      let loadingTask = pdfjsLib.getDocument(reader.result);
+      loadingTask.promise.then(function(pdf) {
+        pdf.getPage(1).then(function(page) {
+          page.getTextContent().then(function(textContent) {
+            for (let i = 0; i < textContent.items.length; i++) {
+              text += textContent.items[i].str + " ";
             }
+            textArea.value = text;
           });
-        }
+        });
       });
-    };
+    } // check if the file is a DOCX file
+    else if (fileName.endsWith('.docx')) {
+    console.log("its Docx FIle");
+      // use Mammoth to convert the DOCX file to HTML
+      mammoth.convertToHtml({arrayBuffer: file}).then((result) => {
+        // get the HTML content from the result
+        let htmlContent = result.value;
+  
+        // display the HTML content in the textarea
+        let textarea = document.getElementById('textArea');
+        textarea.value = htmlContent;
+      });
+    }
+    else {
+      alert("Please upload a text, PDF or Word (.docx) file.");
+    }
+  };
 
+  if (file) {
     reader.readAsArrayBuffer(file);
-  });
+  }
+});
